670f473fa5b9b5a7e0400bf6e8528ef12ca891fd
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2022-01-14 11:45:17 +0200

    task2 completed

--- src/main/java/netology/authorization/SpringBootRestTask1Application.java
+++ src/main/java/netology/authorization/SpringBootRestTask1Application.java
@@ -1,13 +1,24 @@
 package netology.authorization;
 
+import netology.authorization.resolver.UserHandlerMethodArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class SpringBootRestTask1Application {
+public class SpringBootRestTask1Application implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new UserHandlerMethodArgumentResolver());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestTask1Application.class, args);
     }
+
 
 }
